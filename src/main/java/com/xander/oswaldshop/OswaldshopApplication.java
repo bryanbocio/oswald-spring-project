b9690a59ebb48d4a08c46b1983659eaca2b5791e
bbocio <alexanderbocio@gmail.com>
2024-06-23 16:41:30 -0400

    configuring database and stuff needed

---
+++ src/main/java/com/xander/oswaldshop/OswaldshopApplication.java
@@ -0,0 +1,13 @@
+package com.xander.oswaldshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OswaldshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OswaldshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xander/oswaldshop/OswaldshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xander.oswaldshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OswaldshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
